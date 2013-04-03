25139e9a218685d8a447dd1c68e8cfcb2222cd2e
Luis Flores <luchitoflores84@gmail.com>
2013-04-03 12:03:35 -0500

    LazyDataTable

---
+++ src/java/ec/edu/sga/listcontroller/AnioLectivoListController.java
@@ -0,0 +1,67 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.sga.listcontroller;
+
+import ec.edu.sga.facade.AnioLectivoFacade;
+import ec.edu.sga.modelo.matriculacion.AnioLectivo;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.primefaces.model.LazyDataModel;
+
+/**
+ *
+ * @author lucho
+ */
+
+@Named
+@RequestScoped
+public class AnioLectivoListController extends LazyDataModel<AnioLectivo> {
+
+    private static final long serialVersionUID = 3822900416268191502L;
+    private static final Log log = LogFactory.getLog(AnioLectivoListController.class);
+    
+    @EJB
+    private AnioLectivoFacade ejbFacade;
+    private List<AnioLectivo> resultlist;
+    private AnioLectivo[] anios;
+    private AnioLectivo anioLectivoSelected;
+
+    public AnioLectivoListController() {
+        resultlist = new ArrayList<AnioLectivo>();
+    }
+
+    public List<AnioLectivo> getResultlist() {
+        return resultlist;
+    }
+
+    public void setResultlist(List<AnioLectivo> resultlist) {
+        this.resultlist = resultlist;
+    }
+
+    public AnioLectivo getAnioLectivoSelected() {
+        return anioLectivoSelected;
+    }
+
+    public void setAnioLectivoSelected(AnioLectivo anioLectivoSelected) {
+        this.anioLectivoSelected = anioLectivoSelected;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
